6f6a6adceb7e151bea4bf2e81a0be83c2bdaaec6
KP <kjpessetto@gmail.com>
2021-06-03 21:50:52 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Charts from './components/BarChart'
+import './App.css';
+
+const App = () => {
+  return (
+    <body style={{background: '', color: '', display:'flex', justifyContent:"center"}} >
+      <Charts />
+    </body>
+  
+  )
+}
+
+export default App;
